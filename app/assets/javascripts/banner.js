e42d1bfa83677e5f2719d78fd00c68c788adbcb2
Gyenh <blazeog@hotmail.com>
2018-08-21 17:00:52 +0200

    first setup

---
+++ app/assets/javascripts/banner.js
@@ -0,0 +1,7 @@
+$(function(){
+  setInterval(function(){
+    $(".slideshow ul").animate({marginLeft:-3800},3000,function(){
+      $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+    })
+  }, 5000);
+});
