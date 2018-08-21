5659cc688744a6ee14e5cfe3ae037cd74cb761ec
Gyenh <blazeog@hotmail.com>
2018-08-21 20:44:52 +0200

    banner

--- app/assets/javascripts/banner.js
+++ app/assets/javascripts/banner.js
@@ -1,8 +1,13 @@
-$(function(){
+$(document).ready(function () {
+  $(function(){
   setInterval(function(){
     $(".slideshow ul").animate({marginLeft:-3800},3000,function(){
       $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
     })
   }, 5000);
-});
+});});
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
